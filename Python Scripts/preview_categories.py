691841fd9c5313c53eddae9f22e9945a481df0bb
Gavin Wilson <gmanwilson@icloud.com>
2025-09-26 16:08:26 -0400

    Added python dependencies & formatting

--- Python Scripts/preview_categories.py
+++ Python Scripts/preview_categories.py
@@ -9,13 +9,12 @@ for file in os.listdir(data_dir):
     if file.endswith(".csv"):
         file_path = os.path.join(data_dir, file)
         print(f"\n=== {file} ===")
-        
+
         try:
             # Only load 5 rows
             df = pd.read_csv(file_path, nrows=50)
-            df.to_csv(os.path.splitext(file)[0] + "_10_rows" + ".csv", index=False)
-            
-    
+            df.to_csv(os.path.splitext(file)[
+                      0] + "_10_rows" + ".csv", index=False)
+
         except Exception as e:
             print(f"Error reading {file}: {e}")
-
